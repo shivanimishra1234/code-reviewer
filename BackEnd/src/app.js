fef84c5582ba2ca17046d778364d6572668e61b6
shivanimishra1234 <156179416+shivanimishra1234@users.noreply.github.com>
2025-05-23 23:00:57 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://your-vercel-app.vercel.app'], // Add your vercel domain later
+    origin: ['http://localhost:3000', 'https://code-reviewer-ximg.vercel.app/'], // Add your vercel domain later
     credentials: true
 }));
 app.use(express.json({ limit: "50mb" }));

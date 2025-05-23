47eed9b104e97342a17ea78244cb744d9fb2608e
shivanimishra1234 <156179416+shivanimishra1234@users.noreply.github.com>
2025-05-23 21:38:58 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,23 +1,9 @@
-// const app = require('./src/app')
-// require('dotenv').config()
-
-// app.listen(3000,()=>{
-//     console.log('Server is running on port 3000')  // log the server listening message to the console
-// })
-
-
 import app from './src/app.js';
 import dotenv from 'dotenv';
-import express from 'express'
-import cors from "cors";
-app.use(cors());
-
 
 dotenv.config();
 
-app.use(express.json())
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
